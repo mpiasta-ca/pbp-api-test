27b6af333e557786336800d6f1c496e79aa6e26d
Code Monkey <github@mpiasta.ca>
2014-09-19 22:35:53 -0700

    Dev Updates
    New parts built. Still going. Almost finished

--- app/assets/js/restaurants.js
+++ app/assets/js/restaurants.js
@@ -1,38 +1,73 @@
 (function(){
 
     // Init Module
-    var app = angular.module('Restaurants',[]);
+    var module = angular.module('Restaurants', ['ngAnimate']);
 
     // Search Controller
-    app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
-        
+    module.controller('SearchController', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
         // Init vars
         $scope.businesses = [];
         $scope.history = [];
+        $scope.alert = [];
         
-        // On submit
-        $scope.search = function () {
-            $scope.loading = true;
-            $scope.businesses = [];
-            $scope.history.push(angular.copy($scope.params));
-            console.log($scope.history);
-            
-            $http.get('/yelp', {params: $scope.params}).
-            success(function(data){
-                $scope.loading = false;
-                $scope.businesses = data;
-            }).
-            error(function(data){
-                //console.log(data);
-            });
-        };
-        
-        // Populate default search
+        // Init default search
         $scope.params = {
             keywords: 'McDonalds',
             location: '1168 Hamilton, Vancouver, BC',
             radius: '3000'
         };
-    }]);
-    
+        
+        // Form Submit
+        $scope.search = function (inject) {
+            if (inject) {
+                $scope.params = inject;
+            }
+           
+            // Skip Loading if Equal Query
+            if (angular.equals($scope.history[0], $scope.params)) {
+                return $scope.message("warning", "Oops!", "This search is already being displayed.");
+            }
+            
+            // Init Query UI
+            $scope.loading = true;
+            $scope.alert.show = false;
+            $scope.businesses = [];
+            $scope.history.unshift(angular.copy($scope.params));
+            
+            // Limit history length
+            if ($scope.history.length > 5) {
+                $scope.history.pop();
+            }
+            
+            // Yelp Call
+            $http.get('/yelp', {params: $scope.params}).
+                success(function(data){
+                    $scope.loading = false;
+                    $scope.businesses = data;
+                }).
+                error(function(data){
+                    $scope.loading = false;
+                    //console.log(data);
+                });
+        };
+        
+        // Display Message
+        $scope.message = function (type, header, body) {
+            // Set alert data
+            $scope.alert = {
+                show: true,
+                type: type,
+                header: header,
+                body: body
+            };
+            
+            // Cancel existing timer
+            $timeout.cancel($scope.alertTimer);
+            
+            // Auto-hide after 5 seconds
+            $scope.alertTimer = $timeout(function(){
+               $scope.alert.show = false;
+            }, 5000);
+        };
+    }]);    
 })();
