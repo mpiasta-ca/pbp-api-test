44c09bbaf34fbd081d905624d964313405bbf08b
Code Monkey <github@mpiasta.ca>
2014-09-19 18:18:30 -0700

    Update git package
    Update gitignore, remove .vagrant folder, .remove node_modules, and
    automate npm install for dependencies for app on vagrant up

--- puphpet/files/exec-once/0-install.sh
+++ puphpet/files/exec-once/0-install.sh
@@ -2,3 +2,4 @@
 sudo npm install pm2 -g --unsafe-perm 2&>1 >/dev/null
 sudo npm install sails -g 2&>1 >/dev/null
 sudo npm install grunt-cli -g 2&>1 >/dev/null
+cd /var/www/app && sudo npm install 2&>1 >/dev/null
